9103df4565abe0186f9b1e3a8535dbfed6f44622
the8Proffesional <adil8adraibi@gmail.com>
2025-09-27 23:21:26 +0100

    Bienvenue

---
+++ Site/app.py
@@ -0,0 +1,14 @@
+from flask import  Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def home():
+        return render_template("index.html")
+    
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True) 

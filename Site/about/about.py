ce7b3f90914028a556202639b093dcfdebfc362d
the8Proffesional <adil8adraibi@gmail.com>
2025-09-28 00:34:37 +0100

    add about page

---
+++ Site/about/about.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+
+about = Blueprint('about', __name__, template_folder='templates')
+@about.route('/about')
+def about_page():
+    return render_template('about.html')   
--- Site/app.py
+++ Site/app.py
@@ -1,11 +1,12 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template, url_for
 from main.main import main
+from about.about import about
 
 def create_app():
     app = Flask(__name__)
     
     app.register_blueprint(main, url_prefix='/home')
-
+    app.register_blueprint(about, url_prefix='/about')
     @app.route('/')
     def home():
         return render_template("index.html")

01b731d9e0ce02269117d81233ef210360d8b848
the8Proffesional <adil8adraibi@gmail.com>
2025-10-08 01:39:13 +0100

    add utilisateur

--- Site/dashbord/utilisateur.py
+++ Site/dashbord/utilisateur.py
@@ -11,15 +11,28 @@ def manage_Utilisateurs():
     arrondissements = Arondissement.query.all()
     if 'user' not in session:
         return redirect(url_for('admin.admin'))
-    departements=[]
     if request.method == 'POST':
-        arrondissement_id = request.form['arrondissementName']
-        departements = Departement.query.filter_by(arondissement=arrondissement_id).all()
-        if 'departementName' in request.form:
-            departement_id = int(request.form['departementName'])
+        if 'arrondissementName' in request.form:
+            arrondissement_id = request.form['arrondissementName']
             departements = Departement.query.filter_by(arondissement=arrondissement_id).all()
-            return render_template('manage_Utilisateurs.html', name=session['user'], arrondissements=arrondissements, departements=departements, selected_arrondissement=int(arrondissement_id), selected_departement=int(departement_id))
-        else:
             return render_template('manage_Utilisateurs.html', name=session['user'], arrondissements=arrondissements, departements=departements, selected_arrondissement=int(arrondissement_id), selected_departement=-1)
+        if 'departementName' in request.form:
+            departement_id = request.form['departementName']
+            arrondissement_id = request.form['arrondissementName1']
+            departements = Departement.query.filter_by(arondissement=arrondissement_id).all()
+            utilisateurs = Utilisateur.query.filter_by(departement=departement_id).all()
+            return render_template('manage_Utilisateurs.html', name=session['user'], arrondissements=arrondissements, selected_arrondissement=int(arrondissement_id), departements=departements, selected_departement= int(departement_id), utilisateurs=utilisateurs)
+        if 'utilisateurName' in request.form:
+            departement_id = request.form['departementName1']
+            NomUtilisateur = request.form['utilisateurName']
+            arrondissement_id = request.form['arrondissementName2']
+            departements = Departement.query.filter_by(arondissement=arrondissement_id).all()
+            newUtilisateur = Utilisateur(nomUtilisateur=NomUtilisateur, departement=departement_id)
+            db.session.add(newUtilisateur) 
+            db.session.commit()
+            utilisateurs = Utilisateur.query.filter_by(departement=departement_id).all()
+            return render_template('manage_Utilisateurs.html', name=session['user'], arrondissements=arrondissements, departements=departements, selected_arrondissement=int(arrondissement_id), selected_departement=int(departement_id), utilisateurs=utilisateurs)
         
+    
+    
     return render_template('manage_Utilisateurs.html', name=session['user'], arrondissements=arrondissements, selected_arrondissement=-1)

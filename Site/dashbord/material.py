87ce73394b2ca7bda26a052177b1962ef6242402
the8Proffesional <adil8adraibi@gmail.com>
2026-03-26 23:01:43 +0100

    update and delete material

--- Site/dashbord/material.py
+++ Site/dashbord/material.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask import session
 from models import   Marque, MaterialType, Material, Model, Utilisateur
@@ -49,3 +51,31 @@ def manage_Materials():
 
         return render_template('Manage_Material.html',  name=session['user'], materials=materials, submitted=True, models=models, utilisateurs=utilisateurs, selected_Model=Model_id, selected_User=user_id, list=modelList)
     return render_template('Manage_Material.html',  name=session['user'], materials=materials, models=models, utilisateurs=utilisateurs, submitted=False, list=modelList)
+
+@material.route('/update_material/<int:material_id>', methods=['GET', 'POST'])
+def update_material(material_id):
+    if 'user' not in session:
+        return redirect(url_for('adminstration.login')) 
+    material = Material.query.get_or_404(material_id)
+    models = Model.query.all()
+    utilisateurs = Utilisateur.query.all()
+    userid= Utilisateur.query.get(material.utilisateur_id).id
+    if request.method == 'POST':
+        material.inventaire_number = request.form.get('ort')
+        material.serial_number = request.form.get('sn')
+        material.model_id = request.form.get('ModelId', type=int)
+        material.utilisateur_id = request.form.get('UserId', type=int)
+        db.session.commit() 
+        return redirect(url_for('material.manage_Materials'))
+    return render_template('update_selected_material.html', name=session['user'], material=material, models=models, utilisateurs=utilisateurs, selected_Model=material.model_id, selected_User=userid)
+
+
+@material.route('/delete_material/<int:material_id>', methods=['GET'])
+def delete_material(material_id):
+    if 'user' not in session:
+        return redirect(url_for('adminstration.login')) 
+    material = Material.query.get(material_id)
+    if material:
+        db.session.delete(material)
+        db.session.commit()
+    return redirect(url_for('material.manage_Materials'))

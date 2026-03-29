0489bfcb9cba040b062a26cb0ec6c7b73dedcd94
the8Proffesional <adil8adraibi@gmail.com>
2026-03-29 01:38:13 +0100

    ajout nouveau material

--- Site/dashbord/material.py
+++ Site/dashbord/material.py
@@ -1,8 +1,6 @@
-from pyexpat import model
-
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask import session
-from models import   Marque, MaterialType, Material, Model, Utilisateur
+from models import Arondissement,  Departement, Marque, MaterialType, Material, Model, Utilisateur
 from extentions import db   
 
 
@@ -11,46 +9,92 @@ material = Blueprint('material', __name__, template_folder='templates')
 @material.route('/manage_Materials', methods=['GET', 'POST'])
 def manage_Materials():
     if 'user' not in session:
-        return redirect(url_for('adminstration.login')) 
-    materials = Material.query.all()
-    models = Model.query.all()
-    utilisateurs = Utilisateur.query.all()
-    modelList=[]
-    for material in materials:
-        model = Model.query.get(material.model_id)
-        MaterialTypeName = MaterialType.query.get(model.type_material_id)
-        marqueName = Marque.query.get(model.marque_id)
-        user_name = Utilisateur.query.get(material.utilisateur_id)
-        modelList.append([material, model.modelname, MaterialTypeName.typeMaterial, marqueName.MarqueName, user_name.nomUtilisateur])
-
-
+        return redirect(url_for('adminstration.login'))
+    
+    arrondissements = Arondissement.query.all()
+    types=MaterialType.query.all()
     if request.method == 'POST':
-        inventaire_number = request.form.get('ort')
-        SerialNumber = request.form.get('sn')
-        Model_id = request.form.get('ModelId', type=int)
-        user_id = request.form.get('UserId', type=int)
         
-        
-        newMaterial = Material(
-            serial_number=SerialNumber,
-            inventaire_number=inventaire_number,
-            model_id=Model_id,
-            utilisateur_id=user_id
-        )
-        db.session.add(newMaterial)
-        db.session.commit() 
-        materials = Material.query.all()
-        modelList=[]
-        for material in materials:
-            model = Model.query.get(material.model_id)
-            MaterialTypeName = MaterialType.query.get(model.type_material_id)
-            marqueName = Marque.query.get(model.marque_id)
-            user_name = Utilisateur.query.get(material.utilisateur_id)
-            modelList.append([material, model.modelname, MaterialTypeName.typeMaterial, marqueName.MarqueName, user_name.nomUtilisateur])
-        
+        if 'arondissementId' in request.form and 'TypeId' not in request.form:
+            arrondissementId = request.form.get('arondissementId', type=int)
+            departements = Departement.query.filter_by(arondissement=arrondissementId).all()
+            return render_template('Manage_Material.html',  name=session['user'], submitted=False, selected_arrondissement=arrondissementId, arrondissements=arrondissements, departements=departements)
 
-        return render_template('Manage_Material.html',  name=session['user'], materials=materials, submitted=True, models=models, utilisateurs=utilisateurs, selected_Model=Model_id, selected_User=user_id, list=modelList)
-    return render_template('Manage_Material.html',  name=session['user'], materials=materials, models=models, utilisateurs=utilisateurs, submitted=False, list=modelList)
+        if 'DepartementId'  in request.form:
+            arrondissementId = request.form.get('arrondissementName1', type=int)
+            departements = Departement.query.filter_by(arondissement=arrondissementId).all()
+            departementId = request.form.get('DepartementId', type=int)
+            utilisateurs = Utilisateur.query.filter_by(departement=departementId).all()
+            return render_template('Manage_Material.html',  name=session['user'], submitted=False,  arrondissements=arrondissements, departements=departements, utilisateurs=utilisateurs, selected_Departement=departementId, selected_arrondissement=arrondissementId)
+
+
+        if 'UserId' in request.form:
+            arrondissementId = request.form.get('arrondissementName1', type=int)
+            departementId = request.form.get('departementName1', type=int)
+            userId = request.form.get('UserId', type=int)
+            departements = Departement.query.filter_by(arondissement=arrondissementId).all()
+            utilisateurs = Utilisateur.query.filter_by(departement=departementId).all()
+            materials = Material.query.filter_by(utilisateur_id=userId).all()
+            modelList=[]
+            for material in materials:
+                model = Model.query.get(material.model_id)
+                MaterialTypeName = MaterialType.query.get(model.type_material_id)
+                marqueName = Marque.query.get(model.marque_id)
+                user_name = Utilisateur.query.get(material.utilisateur_id)
+                modelList.append([material, model.modelname, MaterialTypeName.typeMaterial, marqueName.MarqueName, user_name.nomUtilisateur])
+
+            
+            return render_template('Manage_Material.html',  name=session['user'], submitted=False, arrondissements=arrondissements, departements=departements, utilisateurs=utilisateurs, selected_User=userId, selected_Departement=departementId, selected_arrondissement=arrondissementId, types=types, list=modelList)
+        
+        if 'TypeId' in request.form:
+            TypeId = request.form.get('TypeId', type=int)
+            arrondissementId = request.form.get('arrondissementName1', type=int)
+            departementId = request.form.get('departementName1', type=int)
+            userId = request.form.get('UserId1', type=int)
+            departements = Departement.query.filter_by(arondissement=arrondissementId).all()
+            utilisateurs = Utilisateur.query.filter_by(departement=departementId).all()
+            materials = Material.query.filter_by(utilisateur_id=userId).all()
+            modelList=[]
+            for material in materials:
+                model = Model.query.get(material.model_id)
+                MaterialTypeName = MaterialType.query.get(model.type_material_id)
+                marqueName = Marque.query.get(model.marque_id)
+                user_name = Utilisateur.query.get(material.utilisateur_id)
+                modelList.append([material, model.modelname, MaterialTypeName.typeMaterial, marqueName.MarqueName, user_name.nomUtilisateur])
+            models = Model.query.filter_by(type_material_id=TypeId).all()
+            Model_id = request.form.get('ModelId', type=int)
+            return render_template('Manage_Material.html',  name=session['user'], submitted=False, arrondissements=arrondissements, departements=departements, utilisateurs=utilisateurs, selected_User=userId, selected_Departement=departementId, selected_arrondissement=arrondissementId, types=types, list=modelList, models=models, selected_Type=TypeId, selected_Model=Model_id)
+        
+        if 'ModelId' in request.form and 'UserId1' in request.form:
+            arrondissementId = request.form.get('arrondissementName1', type=int)
+            departementId = request.form.get('departementName1', type=int)
+            userId = request.form.get('UserId1', type=int)
+            departements = Departement.query.filter_by(arondissement=arrondissementId).all()
+            utilisateurs = Utilisateur.query.filter_by(departement=departementId).all()
+            inventaire_number = request.form.get('ort')
+            SerialNumber = request.form.get('sn')
+            Model_id = request.form.get('ModelId', type=int)
+            user_id = request.form.get('UserId1', type=int)
+            
+        
+            newMaterial = Material(
+                serial_number=SerialNumber,
+                inventaire_number=inventaire_number,
+                model_id=Model_id,
+                utilisateur_id=user_id
+            )
+            db.session.add(newMaterial)
+            db.session.commit()
+            materials = Material.query.filter_by(utilisateur_id=userId).all()
+            modelList=[]
+            for material in materials:
+                model = Model.query.get(material.model_id)
+                MaterialTypeName = MaterialType.query.get(model.type_material_id)
+                marqueName = Marque.query.get(model.marque_id)
+                user_name = Utilisateur.query.get(material.utilisateur_id)
+                modelList.append([material, model.modelname, MaterialTypeName.typeMaterial, marqueName.MarqueName, user_name.nomUtilisateur])
+            return render_template('Manage_Material.html',  name=session['user'], submitted=False, arrondissements=arrondissements, departements=departements, utilisateurs=utilisateurs, selected_User=userId, selected_Departement=departementId, selected_arrondissement=arrondissementId, types=types, list=modelList)
+    return render_template('Manage_Material.html',  name=session['user'], submitted=False, arrondissements=arrondissements)
 
 @material.route('/update_material/<int:material_id>', methods=['GET', 'POST'])
 def update_material(material_id):
